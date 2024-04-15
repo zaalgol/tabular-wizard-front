33b4a4dfbd5a307fb4c22f3132214d374c861164
zaalgol <zaalgol@gmail.com>
2024-04-15 09:25:00 +0300

    will navegate to login if token not valid

---
+++ src/app/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { AuthContext } from '../authentication/AuthContext';
+
+const PrivateRoute = () => {
+    const { isAuthenticated } = useContext(AuthContext);
+
+    return isAuthenticated ? <Outlet  /> : <Navigate to="/login" />
+};
+
+export default PrivateRoute;
+
--- src/app/Routers.js
+++ src/app/Routers.js
@@ -9,16 +9,20 @@ import MainPage from "./MainPage";
 import TrainModel from "../ai/TrainModel"
 import UserModels from "../ai/UserModels"
 import Inference from "../ai/Inference"
+import PrivateRoute from './PrivateRoute';
 
 function Routers() {
   return (
     <Routes>
       <Route path="login" element={<Login />} />
-      <Route path="/" element={<MainPage />} />
-      <Route path="/trainModel" element={<TrainModel />} />
-      <Route path="/userModels" element={<UserModels />} />
-      <Route path="/inference" element={<Inference />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route exact element={<PrivateRoute />}>
+          <Route exact path="/" element={<MainPage />} />
+          <Route path="/trainModel" element={<TrainModel />} />
+          <Route path="/userModels" element={<UserModels />} />
+          <Route path="/inference" element={<Inference />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+      
     </Routes>
   );
 }
